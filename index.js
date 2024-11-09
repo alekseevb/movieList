2957dbc9a71cd2dc58d752722cb83fa1e9ad2c89
Алексей Алексеев <alexseev617@gmail.com>
2024-11-09 13:52:23 +0300

    Все работает, кроме кнопки удаления

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
-const inputNode = document.getElementById('movieInput')
-const addBtnNode = document.getElementById('addBtn');
-const moviesList = document.getElementById('moviesList')
+const PRINT_FILM = 'Введите фильм';
 
-const movieList = [];
+const inputNode = document.getElementById('movieInput');
+const addBtnNode = document.getElementById('addBtn');
+const moviesList = document.getElementById('moviesList');
+const cleanBtnNode = document.getElementById('cleanBtn');
+
+const list = [];
 
 // 1. Добавить стили для зачеркивания
 // 1. Добавлять класс с зачеркиванием
 // 2. Добавить кнопку для обработки удаления
-
+// 3. Серая подлложка остается не изменной, а филмы могут скроллиться
 
 const renderMovie = () => {
     let postHTML = '';
 
-    movieList.forEach((movie) => {
+    list.forEach((movie) => {
         postHTML += `
         <li id="movieItem" class="items__item ">
             <label id="check" class="check">
                 <input type="checkbox" class="check__box">
                 ${movie}
             </label>
-            <button class="items__item-btn">
+            <button id="cleanBtn" class="items__item-btn">
                 <img src="./img/delete.png" alt="delete" class="">
              </button>
         </li>
@@ -36,15 +39,22 @@ const addButtonHandler = () => {
     const currentValue = valueFromUser();
 
     if (!currentValue) {
-        alert('Введите фильм')
+        alert(PRINT_FILM)
         return;
     }
 
-    movieList.push(currentValue);
+    list.push(currentValue);
 
     cleanValueFromUser();
     renderMovie()
 }
 
+const cleanButtonHandler = () => {
+    list.forEach((movie) => {
+        postHTML = '';
+    })
+}
 
-addBtnNode.addEventListener('click', addButtonHandler)
+
+addBtnNode.addEventListener('click', addButtonHandler);
+cleanBtnNode.addEventListener('click', cleanButtonHandler);
